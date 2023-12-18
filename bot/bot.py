55eeb51f218329711d53bdb3127041e5b4273e3d
Denis <drakonniravgol@gmail.com>
2023-12-19 00:44:37 +0300

    forgot del import

--- bot/bot.py
+++ bot/bot.py
@@ -1,7 +1,12 @@
 from aiogram import Dispatcher, Bot
-from database import storage
+from aiogram.fsm.storage.redis import RedisStorage
+from redis.asyncio.client import Redis
 from data.config import Config
 
 config = Config()
+
+redis = Redis.from_url(config.REDIS_URL)
+storage = RedisStorage(redis=redis)
+
 bot = Bot(token=config.TOKEN, parse_mode='HTML')
 dp = Dispatcher(storage=storage)
--- database/__init__.py
+++ database/__init__.py
@@ -3,6 +3,3 @@ from redis.asyncio.client import Redis
 
 from bot.bot import config
 from .db import Database
-
-redis = Redis.from_url(config.REDIS_URL)
-storage = RedisStorage(redis=redis)
