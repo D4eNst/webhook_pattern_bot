6c1849cbc9ddc0784f62be660bf19912f38beb83
Denis <drakonniravgol@gmail.com>
2023-12-19 00:39:48 +0300

    forgot del import

--- database/__init__.py
+++ database/__init__.py
@@ -1,8 +1,8 @@
 from aiogram.fsm.storage.redis import RedisStorage
 from redis.asyncio.client import Redis
 
-from data.config import REDIS_URL
+from bot.bot import config
 from .db import Database
 
-redis = Redis.from_url(REDIS_URL)
+redis = Redis.from_url(config.REDIS_URL)
 storage = RedisStorage(redis=redis)
